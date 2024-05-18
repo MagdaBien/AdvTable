d225ef8030f23e37c3c9060f5e4d5a1a907b857e
Magdalena Bień <bien@magda.krakow.pl>
2024-05-18 23:28:01 +0200

    build again

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const connectToDB = require("./db");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const mongoose = require("mongoose");
-const helmet = require("helmet");
 
 // start express server
 const app = express();
@@ -20,7 +19,6 @@ connectToDB();
 // add middleware
 app.use(cors());
 app.use(express.json());
-app.use(helmet());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   session({
