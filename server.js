360df21b3a32b5cda549cf9b812d878a1071e2a5
Magdalena Bień <bien@magda.krakow.pl>
2024-05-19 10:13:17 +0200

    fix secret

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   session({
-    secret: process.env.secret,
+    secret: process.env["SECRET"],
     cookie: {
       secure: process.env.NODE_ENV == "production",
     },
