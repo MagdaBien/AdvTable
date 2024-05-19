e62586485d4deb6fdf0e6aea3429ddb52812cac8
Magdalena Bień <bien@magda.krakow.pl>
2024-05-19 10:29:18 +0200

    fix secret db

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 
 const connectToDB = () => {
-  const NODE_ENV = process.env.NODE_ENV;
+  const NODE_ENV = process.env["NODE_ENV"];
+  const DB_PASS = process.env["DB_PASS"];
   let dbUri = "";
 
   if (NODE_ENV === "production") {
     dbUri =
-      "mongodb+srv://magbie1978:${process.env.DB_PASS}@cluster0.ut7tgmr.mongodb.net/adsDB?retryWrites=true&w=majority&appName=Cluster0";
+      "mongodb+srv://magbie1978:${DB_PASS}@cluster0.ut7tgmr.mongodb.net/adsDB?retryWrites=true&w=majority&appName=Cluster0";
   } else {
     dbUri = "mongodb://localhost:27017/adsDB";
   }
