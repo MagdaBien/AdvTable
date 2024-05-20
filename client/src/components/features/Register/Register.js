1fd27bd0a9801804fece4eaa7f919e1edc963b4f
Magdalena Bień <bien@magda.krakow.pl>
2024-05-21 01:08:19 +0200

    app with cooki after pull

--- client/src/components/features/Register/Register.js
+++ client/src/components/features/Register/Register.js
@@ -8,6 +8,7 @@ const Register = () => {
   const [login, setLogin] = useState("");
   const [password, setPassword] = useState("");
   const [tel, setTel] = useState("");
+  const [avatar, setAvatar] = useState(null);
   const [status, setStatus] = useState(null); // null, loading, success, serverError, clientError, loginError
   const navigate = useNavigate();
 
@@ -23,7 +24,6 @@ const Register = () => {
 
     const options = {
       method: "POST",
-      credentials: "include",
       body: fd,
     };
 
