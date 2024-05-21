396635b3a79a55164597455772e579646547d67a
Magdalena Bień <bien@magda.krakow.pl>
2024-05-21 02:18:49 +0200

    cors options

--- server.js
+++ server.js
@@ -12,9 +12,13 @@ const app = express();
 
 // connect to DB
 connectToDB();
+const corsOptions = {
+  origin:
+    "https://f1289947-7722-49d7-937f-4ea697bda01d-00-1fttp15mfftfw.riker.replit.dev/",
+};
 
 // add middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
